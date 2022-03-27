c1ed363903066a54e4fb54113835fece6ef8ed53
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-03-27 00:19:52 -0300

    criei os testes para o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import RenderWithRouter from '../utils/RenderWithRouter';
+
+describe('Teste o componente NotFound', () => {
+  test('Teste se página contém um heading h2', () => {
+    const { getByRole } = RenderWithRouter(<NotFound />);
+    const notFoundText = getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    const { getByRole } = RenderWithRouter(<NotFound />);
+    const imageNotFound = getByRole('img', { name: 'Pikachu crying because the '
+    + 'page requested was not found' });
+    const linkImgNotFound = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imageNotFound).toBeInTheDocument();
+    expect(imageNotFound).toHaveAttribute('src', linkImgNotFound);
+  });
+});
